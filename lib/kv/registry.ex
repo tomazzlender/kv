76255f5c0c66fd3356ace98d1a99c651cd875a93
Stefan Eletzhofer <stefan.eletzhofer@nexiles.com>
2023-03-03 16:09:08 +0100

    Monitor bucket crashes

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -25,22 +25,41 @@ defmodule KV.Registry do
 
   @impl true
   def init(:ok) do
-    {:ok, %{}}
+    refs = %{}
+    names = %{}
+    {:ok, {refs, names}}
   end
 
   @impl true
-  def handle_call({:lookup, name}, _from, names) do
-    {:reply, Map.fetch(names, name), names}
+  def handle_call({:lookup, name}, _from, {_, names} = state) do
+    {:reply, Map.fetch(names, name), state}
   end
 
   @impl true
-  def handle_cast({:create, name}, names) do
+  def handle_cast({:create, name}, {refs, names} = state) do
     if Map.has_key?(names, name) do
-      {:noreply, names}
+      {:noreply, state}
     else
       {:ok, bucket} = KV.Bucket.start_link([])
-      {:noreply, Map.put(names, name, bucket)}
+      ref = Process.monitor(bucket)
+      refs = Map.put(refs, ref, name)
+      names = Map.put(names, name, bucket)
+      {:noreply, {refs, names}}
     end
+  end
+
+  @impl true
+  def handle_info({:DOWN, ref, :process, _, _}, {refs, names}) do
+    {name, refs} = Map.pop(refs, ref)
+    {_, names} = Map.pop(names, name)
+    {:noreply, {refs, names}}
+  end
+
+  @impl true
+  def handle_info(message, state) do
+    require Logger
+    Logger.info "received unknown message: #{inspect(message)}"
+    {:noreply, state}
   end
 
 end
