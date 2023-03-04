c73b730b7cd755b8f2bd02ca97218b73772b8fc8
Stefan Eletzhofer <stefan.eletzhofer@nexiles.com>
2023-03-04 20:05:26 +0100

    Add dynamic supervisor
    Start a dynamic supervisor and use it in the registry
    to create buckets instead of manually creating bucket
    processes.

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -46,7 +46,7 @@ defmodule KV.Registry do
     if Map.has_key?(names, name) do
       {:noreply, state}
     else
-      {:ok, bucket} = KV.Bucket.start_link([])
+      {:ok, bucket} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
       ref = Process.monitor(bucket)
       refs = Map.put(refs, ref, name)
       names = Map.put(names, name, bucket)
